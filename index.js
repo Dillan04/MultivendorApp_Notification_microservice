0b665d3af5bfe68a595a5f9906e1eda24b457ed6
Dillan04 <dillanhammond7@gmail.com>
2025-01-19 21:23:06 +0100

    6th commit

--- index.js
+++ index.js
@@ -3,11 +3,27 @@ const app = express();
 const mongoose = require("mongoose");
 const print = console.log;
 const cors = require("cors");
-const port=process.env.PORT||8004
+const port = process.env.PORT || 8004;
 const notificationRoutes = require("./api/notifications");
 app.use(express.json());
-app.use(cors());
 app.use(express.static(__dirname + "/public"));
+
+// Define the allowed origins
+const allowedOrigins = ['https://multivendorapp-frontend.onrender.com', 'https://multivendorapp-user-microservice.onrender.com']; // Replace with your actual domains
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allow specific HTTP methods
+  credentials: true, // Allow credentials (cookies, Authorization headers)
+};
+
+app.use(cors(corsOptions)); // Apply the CORS middleware with the custom options
 
 const { CreateChannel, SubscribeMessage } = require("./utils");
 
@@ -17,15 +33,13 @@ app.use(express.urlencoded({ extended: true }));
 async function startApp() {
   try {
     await mongoose.connect(process.env.DB_URI);
-    print("Connection sauce");
+    print("Connection successful");
 
     const channel = await CreateChannel();
 
-    
-
     await notificationRoutes(app, channel);
     app.listen(port, () => {
-      console.log(`Notifications is Listening to Port ${port}`);
+      console.log(`Notifications service is listening on Port ${port}`);
     });
   } catch (err) {
     console.log("Failed to start app:", err);
